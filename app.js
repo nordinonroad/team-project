13454a8eff007db44b8c774300b3e3bd530594b0
Arty <a.tirabyan@yandex.ru>
2021-08-04 18:12:21 +0300

    Admin Page & Buttons For Adding Posts Page Are Created

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log('Hello, World!')
+const express = require('express');
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const indexRouter = require('./routes/indexRouter');
+const adminRouter = require('./routes/adminRouter');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cookieParser());
+app.use(session({
+  secret: '8u54trgh9but349rgjoi53eigrpj4wegrjpo',
+  resave: false,
+  saveUninitialized: false,
+  name: 'userLogin',
+  cookie: { secure: false },
+  store: new FileStore({}),
+}));
+
+app.use('/', indexRouter);
+app.use('/admin', adminRouter);
+
+// app.use((req, res, next) => {
+//   const error = createError(404, 'Запрашиваемой страницы не существует на сервере.');
+//   next(error);
+// });
+
+// res.locals.message = err.message;
+// res.locals.error = error;
+
+app.listen(PORT, () => {
+  console.log(`server started PORT: ${PORT}`);
+});
---
+++ public/js/application.js
@@ -0,0 +1,24 @@
+//fetch for login error
+console.log(document.forms)
+const { adminEnteringForm } = document.forms;
+console.log(adminEnteringForm);
+if(adminEnteringForm) {
+  adminEnteringForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const response = await fetch('/admin/newsblogdiv', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({login: adminEnteringForm.login.value, password: adminEnteringForm.password.value}),
+  })
+  const statusOfAuth = await response.text();
+  console.log(statusOfAuth);
+    if(statusOfAuth === 'Unauthorized') {
+      const errorDiv = document.querySelector('#errorMessage');
+      errorDiv.innerText = 'Данные введены некорректно';
+    } else 
+    window.location.replace('/admin/newsblogdiv');
+})
+}
+
